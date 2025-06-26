132875c2ea3d1cadf4c2d75ad6102e389d62d0ae
SantiagoOH21 <soh0621@gmail.com>
2025-06-27 01:25:07 +0200

    create src folder

--- src/index.js
+++ src/index.js
@@ -6,12 +6,16 @@ const PORT = process.env.PORT || 3001;
 const { dbConnection } = require("./config/config");
 const { typeError } = require("./middlewares/errors");
 
+const swaggerUI = require("swagger-ui-express");
+const docs = require("./docs/index");
+
 app.use(express.json());
 dbConnection();
 
 app.use("/users", require("./routes/users"));
 app.use("/posts", require("./routes/posts"));
 app.use("/comments", require("./routes/comments"));
+app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(docs));
 
 app.use("/uploads", express.static("uploads"));
 app.use(typeError);
